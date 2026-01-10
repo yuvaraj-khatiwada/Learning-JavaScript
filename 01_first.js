165e45dbe7676ccefd924b8ff19d03a9a0b1fb13
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-10 21:59:00 +0545

    learning javascript

---
+++ 01_first.js
@@ -0,0 +1,5 @@
+console.log("Helloworld!");
+console.log("Again restart for revision of the java scirpt ");
+console.log("Thers no any shortcuts!!")
+
+// the output of console.log displays in the console section of browswer
---
+++ 01_pr2.js
@@ -0,0 +1,14 @@
+// create a const object called "profile" to store information shown in the pictue 
+// in picture ther is name , followers , following , post, descriptin of instragam pictue 
+
+const profile ={
+    fname:"ram",
+    followers: true,
+    post: 55,
+    about: " this is the profile of ram ", 
+    following : false,
+
+};
+console.log(typeof profile);
+console.log(typeof profile["post"]);
+console.log(profile)
