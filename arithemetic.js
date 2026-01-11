d8a1814c4614bdca9dcab3a36f4057b076935306
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-11 21:13:54 +0545

    done with arithemetic and unary operators

---
+++ arithemetic.js
@@ -0,0 +1,24 @@
+// Arthimetic operatos 
+let a = 10 , b = 5;
+let sum = a + b;
+console.log("a =", a ,"b =", b);
+console.log(sum);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log(" a / b =", a / b);
+console.log(" a % b = ", a % b); // this gives the remainder
+console.log(" a ** b = ", a ** b); // a to the power b
+console.log("a= a + 1", a +=  1); // prints 11
+console.log("b+ 1 = " ,++b); // 6
+a++; // this increment means first bring 11 then increase by 1
+console.log(a); // this prints 12
+console.log(a++); // this is post increment, prints 12 but after print it increse by 1
+console.log(a); // this prints 13
+ --b; // this is preincrement first decrease by 1 then print
+ console.log(b); // prints 5
+ console.log(--b); // 4
+
+ // post increase after the print 
+ // pre prints the result of  decreased or increased value
+// a and b are operants 
+
