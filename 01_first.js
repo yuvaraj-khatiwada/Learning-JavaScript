8ab2964125b586b38fbeb3b4763a0c3bc261e215
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-11 22:42:29 +0545

    Organized files into folders for variables and operators

--- 01_first.js
+++
@@ -1,5 +0,0 @@
-console.log("Helloworld!");
-console.log("Again restart for revision of the java scirpt ");
-console.log("Thers no any shortcuts!!")
-
-// the output of console.log displays in the console section of browswer
