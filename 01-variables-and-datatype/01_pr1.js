e1e158a69e21d1cf38e7e2a0d8eec5b21e2f3f28
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-12 00:01:51 +0545

    Confirmed: Files organized into folders and duplicates removed

---
+++ 01-variables-and-datatype/01_pr1.js
@@ -0,0 +1,10 @@
+// create a const object called "product" to story informatin shown in picture 
+// in picture there is one pen , description , rating , sale, and price
+
+const product = {
+    prd_name: "pen", 
+    price: 50 ,
+    availabel: true,
+    rating: 3,
+};
+console.log(product);
