eb02eb38140a8db7668517c662ab7f9413747d08
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-12 14:46:43 +0545

    done with conditional and ternary operators

---
+++ 02-operators & conditinoal_stat/condition1.js
@@ -0,0 +1,27 @@
+let age = "50";
+if(age<20)
+{
+    console.log("the person is minor");
+}
+else if (age>=20 && age< 50)
+{
+    console.log("The person is young ");
+}
+else{
+ console.log("the person is senior citizen")
+}
+
+let mode = "pink";
+let color;
+if (mode === "dark") {
+    console.log("black");
+}
+else if (mode === "while"){
+    console.log("white");
+}
+else if (mode === "pink") {
+    console.log("pink");
+}
+else {
+    console.log("default");
+}
---
+++ 02-operators & conditinoal_stat/ternary.js
@@ -0,0 +1,5 @@
+// ternary operators
+// condition ? true output: false output
+let age = 11;
+let result = age > 18 ?" Adult" : "not Adult";
+console.log(result);
