386e44cd686d30e51004286e0fabd0062f1403f7
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-12 15:00:40 +0545

    renamed the folder to remove special character causing errors

---
+++ 02-operators-conditional-stat/pr1.js
@@ -0,0 +1,4 @@
+// Get user to input a number using promt("Enter a number:") . check the number 
+//  is multiple of 5 or not 
+// note : alert and prompt function works in the browser so link with html
+alert("hello!");
