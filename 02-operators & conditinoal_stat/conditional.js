5bc371353814d8f5ea40b131d7f2e6feca1e0dcf
Yuvaraj Khatiwada <yuv.khatiwada19@gmail.com>
2026-01-12 14:28:49 +0545

    done little with conditional statement

---
+++ 02-operators & conditinoal_stat/conditional.js
@@ -0,0 +1,19 @@
+// to implement some condition in the code
+// if , if-else, if - elseif - else
+let age = "25";
+if(age>18)
+{
+    console.log("you are eligible for driving license");
+}
+else (age<30)
+{
+    console.log("You are not  eligible for ceo");
+}
+if(age%2==0)
+{
+    console.log("the number is even");
+}
+else
+{
+    console.log("The number is oldd");
+}
